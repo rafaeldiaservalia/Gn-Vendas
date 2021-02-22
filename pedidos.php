a3188a0b5ca08b47c9eff1c0926e8617f20a531c
Rafael Dias <rafaelarlindodias@gmail.com>
2021-02-22 02:03:03 -0300

    add title e removido tags sem uso

--- pedidos.php
+++ pedidos.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
+    <title>Gn-Vendas</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet"href="css/estilo.css">
@@ -13,41 +13,39 @@
     </div>
     <div class="content">
         <h2>Pedidos</h2>
-        <table border="1">
-              <colgroup span="4" class="columns"></colgroup>
-              
-                  <?php
-                    require_once 'classes/Compras.php';
-                    $compra = new Compras();
-                    $resultados = $compra->listarTodasCompras();          
-                    if ($compra) {                     
-                            echo '
-                            <tr>
-                                <th>Nome</th>
-                                <th>Telefone</th> 
-                                <th>Produto</th>
-                                <th>Preço (R$)</th> 
-                                <th>Pagar</th>
-                            </tr>';
-                            $msg = false;
-                            while ($dados = $resultados->fetch()) {
-                                echo '<tr>';
-                                echo '<td>'.$dados['nome_cliente'].'</td>';
-                                echo '<td>'.$dados['telefone_cliente'].'</td>';
-                                echo '<td>'.$dados['nome'].'</td>';
-                                echo '<td> R$ '. number_format($dados['valor'], 2, ',', '.') .'</td>';
-                                echo '<td><a href="'.$dados['link_boleto'].'" target="_blank" class="button">Boleto</a></td>';
-                                echo '</tr>';
-                                $msg = true;
-                            }
-                            if ($msg == false) {
-                                echo 'Nenhuma compra realizada!';
-                            }
-                    } else {
-                        echo 'Erro!';
+        <table>
+            <?php
+            require_once 'classes/Compras.php';
+            $compra = new Compras();
+            $resultados = $compra->listarTodasCompras();          
+            if ($compra) {                     
+                    echo '
+                    <tr>
+                        <th>Nome</th>
+                        <th>Telefone</th> 
+                        <th>Produto</th>
+                        <th>Preço (R$)</th> 
+                        <th>Pagar</th>
+                    </tr>';
+                    $msg = false;
+                    while ($dados = $resultados->fetch()) {
+                        echo '<tr>';
+                        echo '<td>'.$dados['nome_cliente'].'</td>';
+                        echo '<td>'.$dados['telefone_cliente'].'</td>';
+                        echo '<td>'.$dados['nome'].'</td>';
+                        echo '<td> R$ '. number_format($dados['valor'], 2, ',', '.') .'</td>';
+                        echo '<td><a href="'.$dados['link_boleto'].'" target="_blank" class="button">Boleto</a></td>';
+                        echo '</tr>';
+                        $msg = true;
                     }
-                  ?>
-            </table>
+                    if ($msg == false) {
+                        echo 'Nenhuma compra realizada!';
+                    }
+            } else {
+                echo 'Erro!';
+            }
+          ?>
+    </table>
     </div>
     <div class="footer">
         <p>Gn-Vendas <?php echo date("Y") ?> - Todos os direitos reservados</p>
